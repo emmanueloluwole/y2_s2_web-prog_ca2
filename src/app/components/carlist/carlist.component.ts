31a0cb3e5f04ab90b1a3d38bcd9c214ed6e66c8a
Emmanuel Oluwole <s00248432@atu.ie>
2025-03-26 11:28:08 +0000

    first commit

---
+++ src/app/components/carlist/carlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carlist',
+  imports: [],
+  templateUrl: './carlist.component.html',
+  styleUrl: './carlist.component.css'
+})
+export class CarlistComponent {
+
+}
